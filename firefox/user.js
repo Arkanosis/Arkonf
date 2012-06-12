c1ce4180fb36db51ca2782127d8a4f7eab93908b
Jérémie Roquet <arkanosis@gmail.com>
2012-06-12 15:17:12 +0200

    * Firefox  - Add custom browser and addons configuration

---
+++ firefox/user.js
@@ -0,0 +1,59 @@
+/*
+** Firefox
+*/
+user_pref("browser.cache.disk.capacity", 1000000);
+user_pref("browser.download.useDownloadDir", false);
+user_pref("browser.rights.3.shown", true);
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("browser.showQuitWarning", true);
+user_pref("browser.startup.homepage", "http://gmail.com");
+user_pref("browser.startup.page", 3);
+user_pref("browser.tabs.animate", false);
+user_pref("browser.tabs.closeButtons", 2);
+user_pref("browser.urlbar.trimURLs", false);
+
+user_pref("general.smoothScroll", false);
+
+user_pref("network.http.pipelining", true);
+
+user_pref("security.warn_viewing_mixed", false);
+
+user_pref("signon.rememberSignons", false);
+
+/*
+** GreaseMonkey
+*/
+user_pref("greasemonkey.editor", "/udir/jroquet/local/bin/emacs-console");
+
+/*
+** It's all text
+*/
+user_pref("extensions.itsalltext.editor", "/udir/jroquet/local/bin/emacs-console");
+user_pref("extensions.itsalltext.extensions", ".txt,.html,.css,.xml,.xsl,.js,.wiki,.xhtml");
+user_pref("extensions.itsalltext.gumdrop_position", "none");
+user_pref("extensions.itsalltext.hotkey", "0:1:0:0:0:13");
+
+/*
+** LocationBar2
+*/
+user_pref("extensions.locationbar2.alternate.breadcrumb-all", true);
+user_pref("extensions.locationbar2.alternate.domain-strong", true);
+user_pref("extensions.locationbar2.alternate.segments-margin", true);
+
+/*
+** LongURLPlease
+*/
+user_pref("extensions.longurlplease.expandstyle", "full");
+
+/*
+** NoScript
+*/
+user_pref("noscript.allowURLBarJS", true);
+
+/*
+** TabMix Plus
+*/
+user_pref("extensions.tabmix.appearance_tab", 3);
+user_pref("extensions.tabmix.loadOnNewTab.type", 0);
+user_pref("extensions.tabmix.newTabButton", false);
+user_pref("extensions.tabmix.tabs.closeButtons.enable", false);
