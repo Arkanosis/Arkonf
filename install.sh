6586b53d18c80c66477c48f4711e17ea9246fda4
Jérémie Roquet <jroquet@arkanosis.net>
2012-09-09 17:05:40 +0200

    * Install  - Compute system paths at install time

--- install.sh
+++ install.sh
@@ -1,19 +1,14 @@
 #! /bin/sh
 
+# TODO write a better canonize using sed loops; check that there is not remaining "/./" in the canonized path
 installPath=`pwd`/`dirname $0`
 while echo $installPath | grep '\.\.' > /dev/null; do
     installPath=`echo $installPath | sed 's_/\./_/_g ; s_/[^/]\+/\.\./_/_g ; s_/[^/]\+/..$__'`
 done
 
-check()
-{
-    # TODO check canonicalize
-    echo
-}
-
 install()
 {
-    test -e "$2" && printf "\033[31m$2 already exist, skipped\033[0m\n" || ( ln -s "$installPath/$1" "$2" && printf "\033[32m$2 succefully installed\033[0m\n" )
+    ( test -h "$2" || test -e "$2" ) && printf "\033[31m$2 already exist, skipped\033[0m\n" || ( ln -s "$installPath/$1" "$2" && printf "\033[32m$2 succefully installed\033[0m\n" )
 }
 
 printf '\033[33mArkinstall v0.1\n'
@@ -39,7 +34,6 @@ install emacs/.emacs.d ~/.emacs.d
 install gdb/.gdbinit ~/.gdbinit
 
 install python/.pythonrc.py ~/.pythonrc.py
-install python/.pydistutils.cfg ~/.pydistutils.cfg
 
 install pyjab/.pyjabrc ~/.pyjabrc
 
@@ -62,6 +56,17 @@ install xmonad ~/.xmonad
 install xorg/.Xmodmap ~/.Xmodmap
 install xorg/.Xresources ~/.Xresources
 install xorg/.Xresources ~/.Xdefaults
+
+if ! ( test -h ~/.pydistutils.cfg || test -e ~/.pydistutils.cfg ); then
+    install python/.pydistutils.cfg ~/.pydistutils.cfg
+    sed -i "s@^prefix=.*@prefix=$HOME/local@" ~/.pydistutils.cfg
+fi
+
+if ! ( test -h ~/.config.site || test -e ~/.config.site ); then
+    install autoconf/.config.site ~/.config.site
+    arch=`uname -sm | tr ' ' '-'`
+    echo "prefix=$HOME/local_$arch" >> ~/.config.site
+fi
 
 echo
 
