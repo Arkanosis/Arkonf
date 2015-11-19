3a7146a1416c082d4523931550282f8e3faca5dd
Jérémie Roquet <jroquet@arkanosis.net>
2015-11-19 19:30:52 +0100

    Enable custom prompt and pretty printing in interactive CPython

--- python/.pythonrc.py
+++ python/.pythonrc.py
@@ -2,31 +2,48 @@ import atexit
 import os
 import readline
 import rlcompleter
+import sys
 
 _history = os.path.expanduser('~/.zcache/python_history')
 
-readline.parse_and_bind('set show-all-if-ambiguous on')
-readline.parse_and_bind('tab: complete')
-
+# History
 if os.path.exists(_history):
-        readline.read_history_file(_history)
+	readline.read_history_file(_history)
 
 readline.set_history_length(-1)
 
 def clean_and_save_history(history):
-        import readline
-        commands = set()
-        for commandId in xrange(readline.get_current_history_length(), 0, -1):
-                command = readline.get_history_item(commandId)
-                if command in commands:
-                        readline.remove_history_item(commandId - 1)
-                else:
-                        commands.add(command)
+	import readline
+	commands = set()
+	for commandId in xrange(readline.get_current_history_length(), 0, -1):
+		command = readline.get_history_item(commandId)
+		if command in commands:
+			readline.remove_history_item(commandId - 1)
+		else:
+			commands.add(command)
 	readline.write_history_file(history)
 
 atexit.register(clean_and_save_history, _history)
+
+# Completion
+readline.parse_and_bind('set show-all-if-ambiguous on')
+readline.parse_and_bind('tab: complete')
+
+# Prompt
+sys.ps1 = '\033[36m>>> \033[0m'
+sys.ps2 = '\033[36m... \033[0m'
+
+# Pretty printing
+def display(value):
+	import pprint
+	if value is not None:
+		import __builtin__
+		__builtin__._ = value
+	pprint.pprint(value, indent=2)
+sys.displayhook = display
 
 del atexit
 del os
 del readline
 del rlcompleter
+del sys
