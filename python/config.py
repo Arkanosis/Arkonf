cfe5cde398c068b5be33dfd19258b599e3571528
Jérémie Roquet <jroquet@arkanosis.net>
2017-11-07 03:46:06 +0100

    Add custom ptpython configuration

---
+++ python/config.py
@@ -0,0 +1,9 @@
+__all__ = (
+    'configure',
+)
+
+def configure(repl):
+    repl.show_signature = True
+    repl.show_docstring = True
+    repl.show_line_numbers = True
+    repl.highlight_matching_parenthesis = True
