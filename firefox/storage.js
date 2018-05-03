fd42874d2cea953de6ef623952ac9abc3bbd774b
Jérémie Roquet <jroquet@arkanosis.net>
2018-05-03 09:00:32 +0200

    Use isolated Firefox containers for Facebook and Amazon

---
+++ firefox/storage.js
@@ -0,0 +1,27 @@
+{
+    "browserActionBadgesClicked": [
+        "6.0.0"
+    ],
+    "onboarding-stage": 5,
+    "containerTabsOpened": 12,
+    "identitiesState@@_firefox-default": {
+        "hiddenTabs": []
+    },
+    "identitiesState@@_firefox-container-2": {
+        "hiddenTabs": []
+    },
+    "identitiesState@@_firefox-container-3": {
+        "hiddenTabs": []
+    },
+    "identitiesState@@_firefox-container-4": {
+        "hiddenTabs": []
+    },
+    "siteContainerMap@@_www.facebook.com": {
+        "userContextId": "3",
+        "neverAsk": true
+    },
+    "siteContainerMap@@_www.amazon.fr": {
+        "userContextId": "4",
+        "neverAsk": true
+    }
+}
