e9f09bf63dfcfc69634f3f85cbe9b2ec95577fe5
Jérémie Roquet <jroquet@arkanosis.net>
2018-06-04 14:44:31 +0200

    Add custom Thunderbird keybindings
    These are closer to Gmail's, and arguably better (most useful features
    do not require a modifier key).

---
+++ thunderbird/user.js
@@ -0,0 +1,24 @@
+/*
+** Thunderbird
+*/
+// Nothing yet
+
+/*
+** KeyConfig
+*/
+user_pref("extensions.dorandoKeyConfig.main.key_archive", "][E][");
+user_pref("extensions.dorandoKeyConfig.main.key_forward", "][F][");
+user_pref("extensions.dorandoKeyConfig.main.key_killThread", "][M][");
+user_pref("extensions.dorandoKeyConfig.main.key_markReadByDate", "!][][");
+user_pref("extensions.dorandoKeyConfig.main.key_markThreadAsRead", "!][][");
+user_pref("extensions.dorandoKeyConfig.main.key_newMessage", "][C][");
+user_pref("extensions.dorandoKeyConfig.main.key_nextMsg", "!][][");
+user_pref("extensions.dorandoKeyConfig.main.key_nextUnreadMsg", "!][][");
+user_pref("extensions.dorandoKeyConfig.main.key_nextUnreadThread", "!][][");
+user_pref("extensions.dorandoKeyConfig.main.key_openMessage", "][O][");
+user_pref("extensions.dorandoKeyConfig.main.key_previousMsg", "!][][");
+user_pref("extensions.dorandoKeyConfig.main.key_previousUnreadMsg", "!][][");
+user_pref("extensions.dorandoKeyConfig.main.key_quit", "!][][");
+user_pref("extensions.dorandoKeyConfig.main.key_reply", "][R][");
+user_pref("extensions.dorandoKeyConfig.main.key_replyall", "][A][");
+user_pref("extensions.dorandoKeyConfig.main.key_toggleRead", "!][][");
