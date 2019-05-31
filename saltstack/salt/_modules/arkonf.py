7a86416a42d7dd694b73c957926d0c69cd11ef31
Jérémie Roquet <jroquet@arkanosis.net>
2019-05-31 02:53:34 +0200

    Restrict known IP addresses and users to locally available ones

--- saltstack/salt/_modules/arkonf.py
+++ saltstack/salt/_modules/arkonf.py
@@ -7,20 +7,21 @@ __virtual_name__ = 'arkonf'
 def __virtual__():
     return __virtual_name__
 
-def allowed_ssh_users(users):
-    return _allowed_users(users, 'ssh', 'sftp')
+def allowed_ssh_users(users, local_users):
+    return _allowed_users(users, local_users, 'ssh', 'sftp')
 
-def allowed_sftp_users(users):
-    return _allowed_users(users, 'sftp')
+def allowed_sftp_users(users, local_users):
+    return _allowed_users(users, local_users, 'sftp')
 
-def _allowed_users(users, *conditions):
+def _allowed_users(users, local_users, *conditions):
     logger.debug('users ({}): {}'.format(conditions, users))
     allowed_users = []
     for user in users:
-        for condition in conditions:
-            if user.get(condition, False):
-                allowed_users.append(user)
-                break
+        if user in local_users:
+            for condition in conditions:
+                if user.get(condition, False):
+                    allowed_users.append(user)
+                    break
     logger.debug('allowed_users ({}): {}'.format(conditions, allowed_users))
     return allowed_users
 
