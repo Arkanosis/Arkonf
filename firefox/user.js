7d656159d2620d3cd0f367f99e41f4eea2a55be0
Jérémie Roquet <jroquet@arkanosis.net>
2025-08-30 20:44:17 +0200

    Disable Firefox's AI crapware

--- firefox/user.js
+++ firefox/user.js
@@ -7,6 +7,8 @@ user_pref("browser.crashReports.unsubmittedCheck.enabled", false);
 user_pref("browser.ctrlTab.recentlyUsedOrder", false);
 user_pref("browser.download.useDownloadDir", false);
 user_pref("browser.messaging-system.whatsNewPanel.enabled", false);
+user_pref("browser.ml.chat.enabled", false);
+user_pref("browser.ml.enable", false);
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
 user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
